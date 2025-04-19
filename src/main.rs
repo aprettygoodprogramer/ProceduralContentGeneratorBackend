c12843c20fef6f06e678d236d27188072d066ae7
Evan <evan.j.ducas@gmail.com>
2025-04-18 20:58:31 -0400

    Added a basic Axum server, with a generatedungeon route.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    Json, Router,
+    extract::{Path, State},
+    http::{HeaderValue, Method},
+    routing::{get, post},
+};
+use dotenv::dotenv;
+use std::env;
+use tokio::net::TcpListener;
+use tower_http::cors::{Any, CorsLayer};
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+
+    let frontend_url = env::var("FRONTEND_URL").unwrap_or_else(|_| "http://localhost:5173".into());
+
+    let cors = CorsLayer::new()
+        .allow_origin(frontend_url.parse::<HeaderValue>().unwrap())
+        .allow_methods([Method::POST, Method::GET])
+        .allow_headers(Any);
+
+    let app = Router::new()
+        .route("/", get(|| async { "Hello, World!" }))
+        .route("/generatedungeon", get(|| async { "Hello, World!" }))
+        .layer(cors);
+
+    let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    println!("listening on port 3000");
+    axum::serve(listener, app).await.unwrap();
 }
